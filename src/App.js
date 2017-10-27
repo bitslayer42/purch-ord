f44ac498726270ecf219c5471f9159f98d16b68e
Jon Wilson <jontwilson@gmail.com>
2017-10-27 09:53:42 -0400

    data structures as object complete:

--- src/App.js
+++ src/App.js
@@ -10,39 +10,58 @@ class App extends Component {
     super(props);
 
     this.state = {
-      calendar: [],
-      features: [], //imported purchase order summary
+      calendar: {},
+      features: [],    //imported purchase order summary
+      arcgis_url: '',
     };  
     //this.onClick = this.onClick.bind(this);
   }
 
   componentDidMount() { 
     this.createCal();
-    this.fetchData();
   }
   
+  //Create 4 weeks of dates in an object
   createCal(){
-    let startDate = moment('2017-06-04T00:00:00.000Z');
-    let calDates = [];
-    for(let i=0;i<28;i++){
-      let dt = moment(startDate).add(i, 'days');
-      calDates.push(
-        {unix: dt.unix(),
-         moment: dt, 
-         dow: i%7,
-         week: parseInt(i/7,10)}
-      )
+    let numDays = 28;
+    let startDateStr = '2017-06-04T00:00:00.000Z';
+    let startDate    = moment(startDateStr);
+    let endDate      = startDate.clone().add(numDays-1, 'days');
+    let endDateStr   = endDate.format("YYYY-MM-DD"); 
+    let calendar = {};
+    let arcgis_url = 'https://arcgis.ashevillenc.gov/arcgis/rest/services/Financials/Financials/FeatureServer/5/query?where='
+      + 'create_date%20between%20%27'
+      + startDateStr
+      + '%27%20and%20%27'
+      + endDateStr
+      + '%27&outFields=create_date&f=json';
+
+    for(let i=0;i<numDays;i++){
+      let dt = startDate.utc().clone().add(i, 'days'); 
+      calendar[dt.unix()*1000] =  //.unix()*1000
+      {
+        moment: dt, 
+        dow: i%7,
+        week: parseInt(i/7,10),
+        count: 0,
+      }
     }
     this.setState({
-      calendar: calDates
-    },()=>{console.log('calendar',this.state.calendar)});
+      calendar,
+      arcgis_url
+    },()=> {
+      //console.log('calendar',this.state.calendar); 
+      this.fetchData();
+    });
   }
   
   //Pull create_date list from Purch Ord Summary
   fetchData(){ 
-    axios.get('https://arcgis.ashevillenc.gov/arcgis/rest/services/Financials/Financials/FeatureServer/5/query?where=create_date%20%3E=%20%272017-06-04T00:00:00.000Z%27&outFields=create_date&f=json')
+    axios.get(this.state.arcgis_url)
     .then(res => { 
       const features = res.data.features; 
+
+      //create assoc array keyed on date with counts as values
       var counts = features.reduce((p, feat) => {
         var date = feat.attributes.create_date; //date in unix timestamp format
         if (!p.hasOwnProperty(date)) {
@@ -54,11 +73,35 @@ class App extends Component {
 
       this.setState({
         features: counts
-      },()=>{console.log('features',this.state.features)});
+      },()=>{
+        console.log('features',this.state.features);
+        this.addCounts();
+      });
     })
     .catch(err => {
       console.log(err);
     }); 
+  }
+
+  addCounts(){
+    const features = this.state.features;
+    let copyCal = Object.assign({}, this.state.calendar); //copy 
+    
+    //merge counts into calendar object
+    Object.keys(features).forEach(function(key) {    
+      copyCal[key].count = features[key]; 
+    });
+
+    //now that we have merged, convert it to array for display
+    let newCal = Object.keys(copyCal).map(function(key) {
+      return copyCal[key];
+    });
+
+    this.setState({
+      calendar: newCal
+    },()=>{
+      console.log('calendar',this.state.calendar); 
+    });
   }
 
   render() {
@@ -70,19 +113,20 @@ class App extends Component {
           Calendar
         </header>
         <div className="App-body">
-          {/* {}
-        {
-          this.state.features
-          //.filter((feature)=>{return feature.properties.ut_station!==0;})
-          //.sort((a,b)=>{return parseInt(a.properties.ut_station, 10) - parseInt(b.properties.ut_station, 10)})
-          .map((feature,ix) => {
-            return (
-              <div key={ix} style={{width:"800px",margin:"0 auto",position:"relative"}}>
-                <div className="App-ut-num">{moment(feature.attributes.create_date).format("MM/DD/YYYY")}</div>
-              </div>
-            )
-          })
-        }          */}
+          <div className="App-cal">
+          {
+            // this.state.calendar
+            // //.filter((feature)=>{return feature.properties.ut_station!==0;})
+            // //.sort((a,b)=>{return parseInt(a.properties.ut_station, 10) - parseInt(b.properties.ut_station, 10)})
+            // .map((date,ix) => {
+            //   return (
+            //     <div key={ix} style={{width:"800px",margin:"0 auto",position:"relative"}}>
+            //       <div className="App-date">{moment(date.moment).format("MM/DD/YYYY")}</div>
+            //     </div>
+            //   )
+            // })
+          } 
+          </div>        
         </div>
       </div>
     );
